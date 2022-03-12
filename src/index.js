949471038304610973f28e3cd74299c9f11a479a
Sławomir Jedlikowski <madafakalol@gmail.com>
2022-03-12 13:05:18 +0100

    feat: root response change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.APP_PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Deployed to EB through GH Actions!');
 });
 
 app.listen(port, () => {
